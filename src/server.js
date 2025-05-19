1ec872fe4396fc159d5534cc342bc954cd0c93ef
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-05-19 07:59:23 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const route1 = require('./auth');
 const route2 = require('./verification');
 const route3 = require('./saveDetails');
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(route1);
 app.use(route2);
 app.use(route3);
+app.use(express.static(path.resolve('public')));
 
 app.listen(3000, () => {
     console.log('Server Listening on port 3000!');
