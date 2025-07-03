d8cbf3ec9b32b37c63ee52d272a5815c7bd93fb3
kRxZykRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2025-07-03 17:43:38 +0100

    Update data.js

--- src/npm/data.js
+++ src/npm/data.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+let data = [];
 
-const funcData = function () {} 
+const funcData = function (url) {
+  if(!data.includes(url)) {
+    data.push(url);
+  }
+}
 router.get('/admin/data', (req, res) => {
   res.json({ data });
 });
 
-module.exports = router;
+module.exports = router, funcData;
